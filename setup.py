787464a72927099ffc03f397df6eada6348e5672
Vineet Kulkarni <54698974+vink246@users.noreply.github.com>
2020-07-25 19:27:07 +0530

    Update version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="socket_messenger",
-    version="v0.0.1-alpha",
+    version="v0.1-alpha",
     author="Vineet Kulkarni",
     author_email="2vineetk@gmail.com",
     description="Makes communication between devices using pubnub through Python easy",
     packages=["socket_messenger"],
     include_package_data=True,
     url = "https://github.com/vink246/socket_messenger.git",
-    download_url = "https://github.com/vink246/socket_messenger/archive/v0.0.1-alpha.tar.gz",
+    download_url = "https://github.com/vink246/socket_messenger/archive/v0.1-alpha.tar.gz",
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
